d55a51375985c8467d8884dbe258dbdd78758db9
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-20 01:05:40 +0000

    remove temp file even on failure

--- scripts/check-spelling-is-sorted.sh
+++ scripts/check-spelling-is-sorted.sh
@@ -3,11 +3,12 @@ tmpfile=sortedchecktmpfile.tmp
 cp $1 $tmpfile
 scripts/sort.sh $tmpfile
 if diff -C3 $1 $tmpfile; then
+    rm $tmpfile
     echo "$1 file is fine"
+    exit 0
 else
+    rm $tmpfile
     echo "$1 file is not sorted!"
     echo "Please run 'scripts/sort.sh $1' to automatically fix it!"
     exit 1
 fi
-rm $tmpfile
-exit 0
