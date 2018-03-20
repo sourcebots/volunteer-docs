46cca6948ee8e446bd73a56666c4c79a1eb31b65
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-20 00:52:18 +0000

    make check-spelling command work with circleci

--- scripts/check-spelling-is-sorted.sh
+++ scripts/check-spelling-is-sorted.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-tmpfile=$(mktemp /tmp/sorted.XXXXXX)
+tmpfile=sortedchecktmpfile.tmp
 cp $1 $tmpfile
 scripts/sort.sh $tmpfile
-git diff --exit-code $1 $tmpfile
+if diff --color -C3 $1 $tmpfile; then
+    echo "$1 file is fine"
+else
+    echo "$1 file is not sorted!"
+    exit 1
+fi
+rm $tmpfile
+exit 0
