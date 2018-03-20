2e7b07cd7a85f4ccfa7b9200bcfdbc44090d29c8
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-20 00:19:59 +0000

    remove old comment

--- scripts/sort-spelling.sh
+++ scripts/sort-spelling.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-spelling_file=.spelling
-# Sort the file (and remove duplicates) (and sort numbering properly)
-sort -u -n -o $spelling_file $spelling_file
-# Remove blank lines
-sed -i '/^$/d' $spelling_file
+scripts/sort.sh .spelling
