8433d667823ecffc4d2502c823e33d766106642f
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-20 01:02:56 +0000

    add a recommendation for fixing the file

--- scripts/check-spelling-is-sorted.sh
+++ scripts/check-spelling-is-sorted.sh
@@ -6,6 +6,7 @@ if diff -C3 $1 $tmpfile; then
     echo "$1 file is fine"
 else
     echo "$1 file is not sorted!"
+    echo "Please run 'scripts/sort.sh $1' to automatically fix it!"
     exit 1
 fi
 rm $tmpfile
