da225eb2819d648b5d4a53227ed88a467dc5493e
Andrew Barrett-Sprot <abarrettsprot@gmail.com>
2018-03-23 17:40:38 +0000

    use --check command on sort

--- scripts/check-spelling-is-sorted.sh
+++ scripts/check-spelling-is-sorted.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-tmpfile=$(mktemp /tmp/sorted.XXXXXXX)
-cp $1 $tmpfile
-scripts/sort.sh $tmpfile
-if diff -C3 $1 $tmpfile; then
-    rm $tmpfile
+#!/bin/sh
+if sort --check --unique $1; then
     echo "$1 file is correctly sorted"
     exit 0
 else
-    rm $tmpfile
     echo "$1 file is not sorted!"
     echo "Please run 'scripts/sort.sh $1' to automatically fix it!"
     exit 1
 fi
+
